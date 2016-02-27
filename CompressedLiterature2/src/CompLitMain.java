5071627df893e3794b2abbe5d7f820b9b5a6e41e
e-rowell <ethan.rowell11@gmail.com>
2016-02-27 05:19:12 -0800

    Merged java doc'd MyHashTable with CompLitMain

--- CompressedLiterature2/src/CompLitMain.java
+++ CompressedLiterature2/src/CompLitMain.java
@@ -8,19 +8,21 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
+import javax.swing.plaf.synth.SynthStyle;
+
+/**
+ * Compressed Literature Main.
+ * 
+ * @author Nicholas Hays & Ethan Rowell
+ * @version Feb 27, 2016
+ * Assignment 4: Compressed Literature2
+ * Presented For: Dr. Chris Marriott
+ */
 public class CompLitMain {
 
 	/**
-	 * this method will carry out compression of a file using the CodingTree class.
+	 * Main entry for program.
 	 * 
-	 * make sure you test with more than one file. ○ Output to two files.  
-	 * Again feel free to hardcode the names of these files into your program.  
-	 * These are the codes text file and the compressed binary file. ○ Display statistics.  
-	 * You must output the original size (in bits), the compressed size (in bits), 
-	 * the compression ratio (as a percentage), the elapsed time for compression, 
-	 * and the hash table statistics. ○ Component testing.  Include all methods used for 
-	 * component testing and debugging.
-		 
 	 * @param args command line args
 	 */
 	public static void main(String[] args) {
@@ -28,7 +30,14 @@ public class CompLitMain {
 		compressText(inputFile);
 	}
 
+	/**
+	 * Compresses the text and outputs the results to file.
+	 * 
+	 * @param inputFile The file to write the compressed text to.
+	 */
 	private static void compressText(File inputFile) {
+		double startTime = System.currentTimeMillis();
+		
 		BufferedReader reader = null;
 		StringBuilder str = new StringBuilder();
 		
@@ -44,12 +53,31 @@ public class CompLitMain {
 		}
 		
 		CodingTree<String> tree = new CodingTree<>(str.toString());
-		writeCodes(tree, str);
-		String binStr = encodeText(tree, str);
+		writeCodes(tree);
+		
+		File compressedFile = new File("compressed.txt");
+		String binStr = encodeText(compressedFile, tree, str);
+		
+		double elapsedTime = (System.currentTimeMillis() - startTime) / 1000;
+		
+		System.out.println(String.format("Uncompressed file size: %d bytes", inputFile.length()));
+		System.out.println(String.format("Compressed file size: %d bytes", compressedFile.length()));
+		System.out.println(String.format("Compression ratio: %.2f%%",
+				(double) compressedFile.length() / inputFile.length() * 100));
+		System.out.println(String.format("Running Time: %.3f seconds \n", elapsedTime));
+		
 		decodeText(tree, binStr);
+		
+		testMethod(tree);
 	}
 
-	private static void writeCodes(CodingTree<String> tree, StringBuilder str) {
+	/**
+	 * Writes the Huffman codes to file.
+	 * 
+	 * @param tree Coding tree that holds the Huffman codes for encoding.
+	 * @param str The string of original characters
+	 */
+	private static void writeCodes(CodingTree<String> tree) {
 		StringBuilder codes = new StringBuilder();
 		codes.append('[');
 		try {
@@ -67,7 +95,15 @@ public class CompLitMain {
 		}
 	}
 	
-	private static String encodeText(CodingTree<String> tree, StringBuilder text) {
+	/**
+	 * Encodes the text to binary using Huffman codes in the tree.
+	 * 
+	 * @param compressedFile The file to write to.
+	 * @param tree Coding tree that holds the Huffman codes for encoding.
+	 * @param text The original text to encode.
+	 * @return the binary string.
+	 */
+	private static String encodeText(File compressedFile, CodingTree<String> tree, StringBuilder text) {
 		String huffString = tree.encodeText(text);	
 		// testMethod(huffString);
 		
@@ -79,7 +115,7 @@ public class CompLitMain {
 			binCodes.add(Integer.parseUnsignedInt(intString, 2));
 		}
 		try {
-			BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream("compressed.txt"));
+			BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(compressedFile));
 			for (int bin32Str : binCodes) {
 				bos.write(bin32Str);
 			}
@@ -90,10 +126,18 @@ public class CompLitMain {
 		}
 		return huffString.toString();
 	}
+	
+	/**
+	 * Decodes the text from the tree given the binary encoded text.
+	 * 
+	 * @param tree Coding tree that holds the Huffman codes for encoding.
+	 * @param encodedText Encoded binary string.
+	 */
 	private static void decodeText(CodingTree<String> tree, String encodedText) {
 		String originalText = tree.decode(encodedText);
+		File decompressedFile = new File("decompressed.txt");
 		try {
-			BufferedOutputStream origBos = new BufferedOutputStream(new FileOutputStream("decompressed.txt"));
+			BufferedOutputStream origBos = new BufferedOutputStream(new FileOutputStream(decompressedFile));
 			char[] chars = originalText.toString().toCharArray();
 			for (char c : chars) {
 				origBos.write(c);
@@ -102,5 +146,38 @@ public class CompLitMain {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	/**
+	 * Test method to test MyHashTable.
+	 * 
+	 * @param tree Coding tree that holds the Huffman codes for encoding.
+	 */
+	private static void testMethod(CodingTree<String> tree) {
+		MyHashTable<Integer, Character> testHashTable = new MyHashTable<>(100);
+		Integer myInteger = new Integer(0);
+		
+		testHashTable.put(myInteger, 'c');
+		
+		// will find key in both instances
+		if (testHashTable.containsKey(myInteger)) System.out.println("Contains: " + myInteger.toString());
+		if (testHashTable.containsKey(0)) System.out.println("Contains: " + 0);
+		
+		// won't find key
+		myInteger = Integer.valueOf(10);
+		if (testHashTable.containsKey(myInteger)) System.out.println("Contains: " + myInteger.toString());
+		
+		// won't find key
+		myInteger = new Integer(10);
+		if (testHashTable.containsKey(myInteger)) System.out.println("Contains: " + myInteger.toString());
+		
+		testHashTable.put(myInteger, 'c');
+		
+		// will find all
+		if (testHashTable.get(myInteger) != null) System.out.println("Found " + myInteger.toString());
+		if (testHashTable.get(Integer.valueOf(10)) != null) System.out.println("Found " + myInteger.toString());
+		if (testHashTable.get(10) != null) System.out.println("Found " + myInteger.toString());
+		
+		System.out.println("Print char: " + testHashTable.get(myInteger));
 	}
 }
